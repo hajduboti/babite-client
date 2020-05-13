946ef6cf0ff53d73981b46b92c7c2598f2292a40
IodizedSalt <christophermoeller2000@gmail.com>
2020-05-13 15:13:45 +0200

    added react-bootstrap and cards for channels

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import Channels from './components/Channels';
-import './App.css';
+import './static/css/App.css';
 import { Provider } from 'react-redux';
 import store from './store';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
