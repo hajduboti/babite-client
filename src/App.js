736953abc88fc25c33d1298d5d69cc6af0bb3750
unknown <boto0018@stud.kea.dk>
2020-05-18 14:27:37 +0200

    change request fulfilled

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Route} from "react-router-dom";
 import Amplify from 'aws-amplify';
 import awsmobile from './aws-exports';
-import Login from './components/auth/Login';
 
 
 Amplify.configure(awsmobile);
@@ -17,7 +16,7 @@ function App() {
   return (
     <Provider store={store}>
       {/* An example of routing/paths that works. Commented for future use. */}
-      <Route path="/login" component={Login} />
+      {/* <Route path="/login" component={Login} /> */}
       <NavbarBabite />
       <Channels/>
     </Provider>
