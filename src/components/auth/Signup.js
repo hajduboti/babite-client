6c38782129b1b448834f2cbbe5ccc2221ee20ff8
IodizedSalt <christophermoeller2000@gmail.com>
2020-05-20 17:24:43 +0200

    changed from verification code to verification link

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -6,10 +6,9 @@ import { Auth } from "aws-amplify";
 class Signup extends Component {
   /* 
     TODO:
-      - verify password fields should make sure passwords match
       - DOB field?
-      - Fix Exit button on signup and login fields
-      -
+      - Fix error message on confirm sign up button click 'network error'
+      - Fix error when clicking confirmation link in email (lambda error)
   
   */
     constructor(props){  
@@ -19,15 +18,9 @@ class Signup extends Component {
             password: "",
             confirmPassword: "",
             email: "",
-            confirmationCode: "",
-            newUser: null
          };  
          this.validateForm = this.validateForm.bind(this)
-         this.validateConfirmationForm = this.validateConfirmationForm.bind(this)
          this.handleSubmit = this.handleSubmit.bind(this)
-         this.handleConfirmationSubmit = this.handleConfirmationSubmit.bind(this)
-         this.renderConfirmationForm = this.renderConfirmationForm.bind(this)
-         this.renderForm = this.renderForm.bind(this)
 
 
     }   
@@ -36,10 +29,6 @@ class Signup extends Component {
       console.log("validated")
         return this.state.username.length > 0 && this.state.password.length > 0 && this.state.password === this.state.confirmPassword;
         }
-
-    validateConfirmationForm() {
-      return this.state.confirmationCode.length > 0;
-    }
 
     async handleSubmit(event) {
       try {
@@ -50,31 +39,16 @@ class Signup extends Component {
               email: this.state.email
           }
           });
-          this.setState({newUser: newUser})
+          alert("A verification link as been sent to your email. Please confirm.")
         } catch (e) {
           alert(e.message);
 
         }
       }
-    async handleConfirmationSubmit(event) {
-        event.preventDefault();
-        try {
-          await Auth.confirmSignUp(this.state.email, this.state.username, this.state.confirmationCode);
-          await Auth.signIn(this.state.email, this.state.password);
-            // window.location = ""
-          } catch (e) {
-            alert(e.message);
-          }
-        }
+    
 
     // Handle changes on fields in the form
     handleChange(event, field) {
-      
-      console.log(this.state.username)
-      console.log(this.state.password)
-      console.log(this.state.confirmPassword)
-      console.log(this.state.email)
-      console.log(this.state.confirmationCode)
         let value = event.target.value;
         switch(field){
           case "username":
@@ -99,91 +73,61 @@ class Signup extends Component {
                 email: value
             }); 
             break;
-
-          case "confirmationcode":
-              this.setState({
-                confirmationCode: value
-            }); 
-            break;
            
         }
     }
-
-    renderConfirmationForm(){
-      return(
-      <form onSubmit={this.handleConfirmationSubmit}>
-      <FormGroup controlId="confirmationCode">
-          <label>Confirmation Code</label>
-          <FormControl
-            autoFocus
-            type="tel"
-            onChange={e => this.handleChange(e, "confirmationcode")}
-            value={this.state.confirmationCode}
-          />
-          <Form.Text>Please check your email for the code.</Form.Text>
-          <Button block disabled={!this.validateConfirmationForm()} type="submit">
-              Confirm
-            </Button>
-        </FormGroup>
-        </form>
-        )
-    }
     renderForm(){
-      return (
-        <div className="Login">
-          <form onSubmit={this.handleSubmit}>
-            {/* Username */}
-            <FormGroup controlId="username" >
-              <label>Username</label>
-              <FormControl
-                autoFocus
-                type="text"
-                value={this.state.username}
-                onChange={e => this.handleChange(e, "username")}
-              />
-            </FormGroup>
-    
-            {/* Password */}
-            <FormGroup controlId="password" >
-              <label>Password</label>
-              <FormControl
-                value={this.state.password}
-                onChange={e => this.handleChange(e, "password")}
-                type="password"
-              />
-            </FormGroup>
-    
-            {/* Confirm Password */}
-            <FormGroup controlId="confirmpassword" >
-              <label>Confirm Password</label>
-              <FormControl
-                value={this.state.confirmPassword}
-                onChange={e => this.handleChange(e, "confirmpassword")}
-                type="password"
-              />
-            </FormGroup>
-            <FormGroup controlId="email" >
-              <label>Email</label>
-              <FormControl
-                value={this.state.email}
-                onChange={e => this.handleChange(e, "email")}
-                type="email"
-              />
-            </FormGroup>
-            
-            <Button block disabled={!this.validateForm()} type="submit">
-              Sign Up
-            </Button>
-          </form>
-        </div>
-      );
+      
     }
 render(){
-  return(
-  <div className="Signup">
-  {this.state.newUser === null ?  this.renderForm() : this.renderConfirmationForm() }
-</div>
-)
+  return (
+    <div className="Login">
+      <form onSubmit={this.handleSubmit}>
+        {/* Username */}
+        <FormGroup controlId="username" >
+          <label>Username</label>
+          <FormControl
+            autoFocus
+            type="text"
+            value={this.state.username}
+            onChange={e => this.handleChange(e, "username")}
+          />
+        </FormGroup>
+
+        {/* Password */}
+        <FormGroup controlId="password" >
+          <label>Password</label>
+          <FormControl
+            value={this.state.password}
+            onChange={e => this.handleChange(e, "password")}
+            type="password"
+          />
+        </FormGroup>
+
+        {/* Confirm Password */}
+        <FormGroup controlId="confirmpassword" >
+          <label>Confirm Password</label>
+          <FormControl
+            value={this.state.confirmPassword}
+            onChange={e => this.handleChange(e, "confirmpassword")}
+            type="password"
+          />
+        </FormGroup>
+        <FormGroup controlId="email" >
+          <label>Email</label>
+          <FormControl
+            value={this.state.email}
+            onChange={e => this.handleChange(e, "email")}
+            type="email"
+          />
+        </FormGroup>
+        
+        <Button block disabled={!this.validateForm()} type="submit">
+          Sign Up
+        </Button>
+      </form>
+    </div>
+  );
 }
 }
 export default Signup;
