4e17b97c6e7e4041ace5654e4dae9991db48431a
IodizedSalt <christophermoeller2000@gmail.com>
2020-05-27 17:10:47 +0200

    calender modal component

--- src/components/EditProgramme.js
+++ src/components/EditProgramme.js
@@ -3,7 +3,7 @@ import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment'
 import { connect } from 'react-redux';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { Container } from 'react-bootstrap';
+import { Container, Button, Modal } from 'react-bootstrap';
 import "../static/css/programme.css";
 
 const localizer = momentLocalizer(moment);
@@ -17,45 +17,90 @@ class EditProgramme extends Component {
       super(props);  
       const events = [{}]
       this.state = {
-        events
+        events,
+        isOpen: false,
+
       };
+      this.renderModal = this.renderModal.bind(this)
     } 
     
     componentDidMount(){
     }
 
-    handleSelect = ({ start, end }) => {
-      const title = window.prompt('New Event name')
-      if (title)
+    // When selecting time range on calendar, toggle isOpen to display popup
+    handleSelect = ({ start, end, url, timestamp }) => {
+      this.setState({isOpen: !this.state.isOpen})
+
+      //This saves event to calendare if valid values
+      if(url){
         this.setState({
           events: [
             ...this.state.events,
             {
-              start,
-              end,
-              title,
+              start, 
+              end, 
+              url,
+              timestamp,
             },
           ],
+          isOpen: false
         })
+      }
+     
+    }
+
+    saveEvent = ({start, end, url, timestamp}) => {
+     this.setState({isOpen: false})
+  
+    }
+
+    renderModal = () => {
+
+      if (!this.state.isOpen){
+        return;
+      } else{
+        return(
+          <Modal
+          keyboard
+          show={this.state.isOpen}
+          onHide={() => this.setState({isOpen: false})}
+          aria-labelledby="ModalHeader"
+        >
+
+          <Modal.Header closeButton>
+          <Modal.Title id="example-custom-modal-styling-title">
+            Enter Content
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <input placeholder="content url"></input>
+            <input placeholder="timestamp"></input>
+            <button onClick={this.saveEvent}>Confirm</button>
+
+          </Modal.Body>
+        </Modal>
+       )
+      }
     }
 
     render() {
-      // console.log(moment())
         return(
+          <div className="container">
+          {this.renderModal()}
           <Container className="calendar-background">
-            <div> 
-                <Calendar
-                selectable
-                events={this.state.events}
-                localizer={localizer}
-                views={['day', 'week']}
-                defaultView='day'
-                onSelectEvent={event => alert(event.title)}
-                onSelectSlot={this.handleSelect}
+            <Calendar
+            selectable
+            events={this.state.events}
+            localizer={localizer}
+            views={['day', 'week']}
+            defaultView='day'
+            onSelectEvent={event => alert(event.title)}
+            onSelectSlot={this.handleSelect}
 
-            />
-            </div>
-            </Container>
+          />
+        </Container>
+        <Button>Confirm Programme</Button>
+        </div>
         )
     }
 
@@ -66,5 +111,4 @@ EditProgramme.propTypes = propTypes
 const mapStateToProps = state => ({
   })
   
-//    export default connect(mapStateToProps, { fetchProgrammes })(EditProgramme);
    export default connect(mapStateToProps, { })(EditProgramme);
