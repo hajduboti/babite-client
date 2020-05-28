13dbff53d690c792b45d524e77c87032d69a0c23
unknown <boto0018@stud.kea.dk>
2020-05-28 13:55:33 +0200

    added tailwind

--- src/components/EditProgramme.js
+++ src/components/EditProgramme.js
@@ -3,74 +3,49 @@ import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment'
 import { connect } from 'react-redux';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { Container } from 'react-bootstrap';
-import "../static/css/programme.css";
-
 
 const localizer = momentLocalizer(moment);
-const date = moment(); // Thursday Feb 2015
-const dow = date.day();
 
-// Start week at Monday rather than Sunday.
-moment.locale('ko', {week: {dow: dow}});
-
-
-const propTypes = {}
 
 class EditProgramme extends Component {
     constructor(props){
       super(props);
-      const events = [{}]
+      const events = [
+        {
+        }
+      ]
       this.state = {
+        name: 'React',
         events
       };
     }
 
     componentDidMount(){
-    }
-
-    handleSelect = ({ start, end }) => {
-      const title = window.prompt('New Event name')
-      if (title)
-        this.setState({
-          events: [
-            ...this.state.events,
-            {
-              start,
-              end,
-              title,
-            },
-          ],
-        })
+    //   this.props.fetchProgrammes();
     }
 
     render() {
-      // console.log(moment())
         return(
-          <Container className="calendar-background">
             <div>
                 <Calendar
-                selectable
                 events={this.state.events}
-                localizer={localizer}
-                views={['day', 'week']}
-                defaultView='day'
-                onSelectEvent={event => alert(event.title)}
-                onSelectSlot={this.handleSelect}
-                format={"DD/MM/YYYY HH:mm"}
                 startAccessor="start"
-                      endAccessor="end"
+                endAccessor="end"
+                defaultDate={moment().toDate()}
+                localizer={localizer}
             />
             </div>
-            </Container>
         )
     }
 
 }
-EditProgramme.propTypes = propTypes
+
+// Setup the localizer by providing the moment (or globalize) Object
+// to the correct localizer.
 
 
 const mapStateToProps = state => ({
+    // channels: state.channels.items
   })
 
 //    export default connect(mapStateToProps, { fetchProgrammes })(EditProgramme);
