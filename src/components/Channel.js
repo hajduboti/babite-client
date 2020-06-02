cf354f37cffed21eadb9d37e53ca3719ebcd1a21
unknown <boto0018@stud.kea.dk>
2020-06-02 15:18:31 +0200

    channel video logic wipo

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -3,21 +3,59 @@ import Player from './Player'
 import { getChannelByName } from '../actions/channelActions';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-
+import moment from 'moment'
 
 class Channel extends Component {
+
+
   componentDidMount(){
     const name = window.location.pathname.replace('/','');
     this.props.getChannelByName(name);
-    const channelData = this.props.getChannelByName(name);
+  }
+
+  getTodaysKeys(programmeKeys){
+    let today = moment.utc().format("YYYY-MM-DD");
+    let keys = [];
+    for(const element of programmeKeys) {
+      if(element.indexOf(today) > -1){
+          keys.push(element)
+      }
+    }
+    return keys.sort()
+  }
+
+  getCurrentProgrammeKey(programmeKeys){
+    let currentProgrammeKey;
+    for(const element of programmeKeys) {
+      let unixKey = moment(element).utc().unix()
+      if(moment().unix() > unixKey){
+        currentProgrammeKey = element
+      }else{
+        return "No programmes"
+      }
+    }
+    return currentProgrammeKey
+  }
+
+  showMedia(currentProgramme){
+    return currentProgramme[0].url
   }
 
   render() {
-    const data = this.props.channel;
+    const channelData = this.props.channel
+    let toDisplay;
+    let currentVideo;
+    if(channelData){
+      const programmeKeysOfDay = this.getTodaysKeys(Object.keys(channelData.programme))
+      if(programmeKeysOfDay){
+        const currentProgrammeKey = this.getCurrentProgrammeKey(programmeKeysOfDay)
+        currentVideo = (this.showMedia(channelData.programme[currentProgrammeKey]))
+      }
+    }
+
     return (
       <div>
-        <Player />
-        {data}
+        <Player url={currentVideo}> </Player>
       </div>
 
 
@@ -26,8 +64,7 @@ class Channel extends Component {
 }
 
 Channel.propTypes = {
-  getChannelByName: PropTypes.func.isRequired,
-  channel: PropTypes.string
+  getChannelByName: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
--- src/components/Player.js
+++ src/components/Player.js
@@ -12,14 +12,14 @@ export default class Player extends Component {
     }
   }
   render() {
+    let url = this.props.url
+
     return (
 
       <Container fluid >
         <Row className="player-row">
           <Col xs={{ span: 8, offset: 2 }} className="player-container">
-            <ReactPlayer width="100%" height="100%" url='https://www.youtube.com/watch?v=3_5HRj32YeQ&ab_channel=BaumgartnerRestoration' playing />
-            {/* This line below is needed, but commented for now*/}
-            {/* <ReactPlayer width="100%" height="100%" url='https://www.twitch.tv/j4ckiechan' playing /> */}
+            <ReactPlayer width="100%" height="100%" url={url} playing />
           </Col>
         </Row>
       </Container>
