358086952038fa240939974e2ad8c5ae761ec43b
IodizedSalt <christopher@procedural.build>
2020-06-02 15:12:42 +0200

    working on updateevent functionality

--- src/components/EditProgramme.js
+++ src/components/EditProgramme.js
@@ -7,7 +7,10 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
 import Grid from '@material-ui/core/Grid';
 import DateFnsUtils from '@date-io/date-fns';
-
+import moment from 'moment'
+import "@fullcalendar/core/main.css";
+import "@fullcalendar/daygrid/main.css";
+import "@fullcalendar/timegrid/main.css";
 import {
   MuiPickersUtilsProvider,
   KeyboardTimePicker,
@@ -23,7 +26,8 @@ export default class EditProgramme extends Component {
     constructor(props){  
       super(props);  
       this.handleChange = this.handleChange.bind(this);
-      this.handleDateChange = this.handleDateChange.bind(this)
+      this.handleDateChange = this.handleDateChange.bind(this);
+      this.handleEventClick = this.handleEventClick.bind(this);
     } 
 
     state = {
@@ -32,21 +36,22 @@ export default class EditProgramme extends Component {
       url: '',
       timestamp: '',
       selectedDate: '',
+      selectedEvent: ''
 
     };
 
     componentDidMount(){
-      this.setState({selectedDate: new Date(), selectedTime: new Date().getTime() })
+      // this.setState({selectedDate: new Date(), selectedTime: new Date().getTime() })
     }
     
 
     render() {
         return(
           <Container className= "container">
-          {this.renderModal()}
+          {this.renderDateSelectModal()}
+          {this.renderEventSelectModal()}
           <FullCalendar
             ref={this.calendarRef}
-            // defaultView="dayGridDay"
             header={{
               left: "prev,next today",
               center: "title",
@@ -59,8 +64,10 @@ export default class EditProgramme extends Component {
             plugins={[ timeGridPlugin, dayGridPlugin, interactionPlugin ]} 
             events={this.state.calendarEvents}
             dateClick={this.saveEvent}
+            eventClick={this.handleEventClick}
             editable={true}
             selectable={true}
+            allDayText={''}
             slotLabelFormat={[
               {
                   hour: '2-digit',
@@ -72,20 +79,42 @@ export default class EditProgramme extends Component {
         </Container>
         )
     }
+
     handleDateClick =(arg) => {
-      if(this.state.isOpen){
+      if(this.state.isNewEventModal){
         this.setState({calendarEvents: this.state.calendarEvents.concat({
           title: this.state.url,
           start: this.state.selectedDate,
           // allDay: arg.allDay
         })
       })
-    }
+    }else if(this.state.isSelectedEvent){
+      // TODO: Replace existing event in array with updated event
+      console.log(this.state.selectedDate)
+      this.setState({calendarEvents: this.state.calendarEvents.concat({
+        title: this.state.url,
+        start: this.state.selectedDate,
+        // allDay: arg.allDay
+      })
+    
+    })
+  }
   };
+  handleEventClick = (calEvent, jsEvent, view) => {
+    if(calEvent.event){
+      let date = calEvent.event.start
+      this.setState({isSelectedEvent: !this.state.isSelectedEvent}, this.handleDateClick())
+      this.setState({selectedEvent: calEvent, selectedDate:date })
+
+    }else{
+      console.log("something went wrong")
+    }
+    }
 
    saveEvent = (arg) => {
+     console.log(arg)
     this.setState({selectedDate: arg.dateStr})
-    this.setState({isOpen: !this.state.isOpen}, this.handleDateClick())
+    this.setState({isNewEventModal: !this.state.isNewEventModal}, this.handleDateClick())
     }
 
   handleChange(event) {
@@ -99,18 +128,70 @@ export default class EditProgramme extends Component {
 
   
   }
+  renderEventSelectModal = (event) => {
+    return(
+      <Modal data-backdrop="false"
+        keyboard
+        show={this.state.isSelectedEvent}
+        onHide={() => this.setState({isSelectedEvent: false})}
+        aria-labelledby="ModalHeader"
+      >
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <Grid container justify="space-around">
+            <KeyboardDatePicker
+              disableToolbar
+              variant="inline"
+              format="MM/dd/yyyy"
+              margin="normal"
+              id="date-picker-inline"
+              label="Date picker inline"
+              value={this.state.selectedDate}
+              onChange={this.handleDateChange}
+              KeyboardButtonProps={{
+                'aria-label': 'change date',
+              }}
+            />
+
+            <KeyboardTimePicker
+              margin="normal"
+              id="time-picker"
+              label="Time picker"
+              ampm={false}
+              value={this.state.selectedDate}
+              onChange={this.handleDateChange}
+              KeyboardButtonProps={{
+                'aria-label': 'change time',
+              }}
+              />
+            </Grid>
+            </MuiPickersUtilsProvider>
+
+              <Modal.Header closeButton>
+              <Modal.Title id="example-custom-modal-styling-title">
+                Edit Content
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <input onChange={this.handleChange} defaultValue={this.state.url}  placeholder="content url" id='url'></input>
+                <input onChange={this.handleChange} defaultValue={this.state.timestamp} placeholder="timestamp" id='time'></input>
+                <button onClick={this.handleEventClick}>Confirm</button>
+      
+              </Modal.Body>
+            </Modal>
+     )
+  }
 
   handleDateChange = (date) => {
     this.setState({selectedDate: date});
   };
 
-  renderModal = () => {
+  renderDateSelectModal = () => {
 
       return(
         <Modal data-backdrop="false"
           keyboard
-          show={this.state.isOpen}
-          onHide={() => this.setState({isOpen: false})}
+          show={this.state.isNewEventModal}
+          onHide={() => this.setState({isNewEventModal: false})}
           aria-labelledby="ModalHeader"
         >
           <MuiPickersUtilsProvider utils={DateFnsUtils}>
